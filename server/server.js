88a7987aee0942e3de189f2ddb7015aabd5022bf
Ryan Wilson <RryanWilsonW@gmail.com>
2022-02-13 13:15:03 -0500

    dotenv pachage installed and set up in server.js, .env file created and in git ignore to hold mongobd information. MongoDB set up and connected to application.

--- server/server.js
+++ server/server.js
@@ -1,22 +1,46 @@
-import express from "express";
+import express, { urlencoded } from "express";
 import mongoose from "mongoose";
+import Cards from "./models/dbCards.js";
+import Cors from "cors";
+import dotenv from "dotenv";
 
 //App config
 const app = express();
 const port = process.env.PORT || 8001;
-const connectionURL =
-  "mongodb+srv://RryanWilsonW:C@rb0n2020@cluster0.puzyn.mongodb.net/tinderdb?retryWrites=true&w=majority";
+dotenv.config({ path: ".env" });
+
 //Middlewares
-
+app.use(express.json());
+app.use(Cors());
 //Db config
-mongoose.connect(connectionURL, {
-  useNewURLParser: true,
-  useCreateIndex: true,
-  useUnifiedTopology: true,
+mongoose.connect(process.env.CONNECTION_URL).then(() => {
+  console.log("database connected");
 });
-
 //API Endpoints
 app.get("/", (req, res) => res.status(200).send("Hello World!!"));
+
+//This will send information to our database
+app.post("/tinder/cards", (req, res) => {
+  const dbCard = req.body;
+  Cards.create(dbCard, (err, data) => {
+    if (err) {
+      res.status(500).send(err);
+    } else {
+      res.status(201).send(data);
+    }
+  });
+});
+console.log(process.env.CONNECTION_URL);
+//This will retrieve information from out database
+app.get("/tinder/cards", (req, res) => {
+  Cards.find((err, data) => {
+    if (err) {
+      res.status(500).send(err);
+    } else {
+      res.status(200).send(data);
+    }
+  });
+});
 
 //Listeners
 app.listen(port, () => console.log(`listening on loacalhost: ${port}`));
